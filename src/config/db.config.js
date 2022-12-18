ebcb2cf913d5554939c6ac5abb37ca838843306a
Faizan7012 <frtoons7012@gmail.com>
2022-12-18 12:13:24 +0530

    new

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.set("strictQuery", false);
 const connect = async()=>{
     return mongoose.connect('mongodb+srv://medshoppe:medshoppe@cluster0.lfujmad.mongodb.net/medshoppe?retryWrites=true&w=majority')
 }
